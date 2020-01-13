f063e37e368c8edae68bcf8af1364cd73788a1c1
Zprogrammer-code <skatboyza124@hotmail.com>
2020-01-13 13:13:36 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Company
     attr_accessor :name, :size
 
@@ -9,8 +10,48 @@ end
 
 companies = [
     Company.new('Alpha', 30),
-    Company.new('Beta, 300),
-    Company.new('Delta, 3000)
+    Company.new('Beta', 300),
+    Company.new('Delta', 3000)
 ]
 
 ### Your code below this
+
+def list(companies)
+    list_of_companies = []
+    companies.map{ |company|
+     list_of_companies << "#{company.name}, #{company.size}"
+}
+list_of_companies
+end
+
+def combine_company_sizes(companies)
+total_size = []
+    companies.map{ |company|
+    total_size << company.size
+}
+total_size.reduce(0){ |sum, each_size|
+sum + each_size
+}
+end
+
+def filter_to_show_size_over_100(companies)
+  size_over_100 =  companies.select{|company| company.size > 100}
+  list(size_over_100)
+ end
+
+ def find_company_named_Beta(companies)
+ beta = companies.find{|company|  if company.name == "Beta"
+return company.name
+ end
+}
+ end
+
+ def the_largest_company(companies)
+companies.max_by do |company|
+company.size
+end
+ end
+
+ def sort_companies_max_to_min(companies)
+companies.sort {|a, b| list(companies).max <=> list(companies).min}
+ end
